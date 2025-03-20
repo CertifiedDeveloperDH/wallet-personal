d1d39252d7761ae1a09162c96764b97358c17c07
certifieddeveloper085 <certifieddeveloper085@gmail.com>
2025-03-20 09:54:12 -0300

    wallet-personal

--- api-cuenta/src/main/java/com/DigitalHouse/Entity/Cuenta.java
+++ api-cuenta/src/main/java/com/DigitalHouse/Entity/Cuenta.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
+
+import static com.DigitalHouse.Entity.AliasGenerator.generateAlias;
+
 @Entity
 @Builder
 @Data
@@ -19,7 +22,7 @@ public class Cuenta {
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String alias;
+    private String alias = generateAlias();
 
     @Column(nullable = false)
     private BigDecimal balance;
