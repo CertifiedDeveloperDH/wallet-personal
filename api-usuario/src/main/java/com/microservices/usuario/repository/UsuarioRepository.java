f8e47e93106af35a916e5ecc9de6576a33966d90
certifieddeveloper085 <certifieddeveloper085@gmail.com>
2025-03-25 07:58:38 -0300

    wallet-personal-testing

--- api-usuario/src/main/java/com/microservices/usuario/repository/UsuarioRepository.java
+++ api-usuario/src/main/java/com/microservices/usuario/repository/UsuarioRepository.java
@@ -3,10 +3,11 @@ package com.microservices.usuario.repository;
 import com.microservices.usuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
     Optional<Usuario> findByEmail(String email);
 
 }
